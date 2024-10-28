b7294b85cc083c895db4f976bb77d703f9b07965
yminanc <marandriamiarijaona@gmail.com>
2024-10-28 20:31:20 +0100

    initialisation de l'interface

---
+++ Interface/pages_uses/cartographie.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.header("Cartographie")
+    st.write("Ici, vous pouvez visualiser la cartographie des biens immobiliers.")
---
+++ Interface/pages_uses/menu.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+def show_menu():
+    st.sidebar.markdown("<h1 style='text-align: center; color: black;'>GreenTech</h1>", unsafe_allow_html=True)
+    
+    # Boutons de navigation dans le menu latéral
+    if st.sidebar.button("🏠 Accueil"):
+        st.session_state.page = 'Accueil'
+    if st.sidebar.button("🗺️ Cartographie"):
+        st.session_state.page = 'Cartographie'
+    if st.sidebar.button("📊 Prédiction prix de vente"):
+        st.session_state.page = 'Prédiction'
+    if st.sidebar.button("📈 Évolution"):
+        st.session_state.page = 'Évolution'
+
+    # Pied de page
+    st.sidebar.markdown("GreenTech Solutions © 2024")
