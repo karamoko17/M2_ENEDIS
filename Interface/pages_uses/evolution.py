470fecd13c6b69f30502942c89e286f2906d4433
ADJARO <119491882+Adjaro@users.noreply.github.com>
2024-11-02 18:56:00 +0100

    update1

--- Interface/pages_uses/evolution.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.header("Évolution des Prix")
-    st.write("Ici, vous pouvez voir l'évolution des prix immobiliers au fil du temps.")
--- Interface/pages_uses/prediction.py
+++
@@ -1,5 +0,0 @@
-import streamlit as st
-
-def show():
-    st.header("Prédiction du Prix de Vente")
-    st.write("Ici, vous pouvez estimer le prix de vente d'un bien immobilier.")
