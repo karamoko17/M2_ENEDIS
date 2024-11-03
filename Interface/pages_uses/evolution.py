f26ac9a06e4b74377d3d4293effbe4d51953e87c
ADJARO <119491882+Adjaro@users.noreply.github.com>
2024-11-03 19:41:53 +0100

    update

--- Interface/pages_uses/evolution.py
+++ Interface/pages_uses/evolution.py
@@ -21,7 +21,7 @@ import plotly.express as px
 # }
 
 # df = pd.DataFrame(data)
-df = pd.read_csv('../data/data_carto.csv')
+df = pd.read_csv('../Data/data_carto.csv')
 
 def show():
     # Filtres dans la barre latérale
