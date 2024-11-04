ed3e24460adfb386a831cc9ef7a2f91ca74d81eb
ADJARO <119491882+Adjaro@users.noreply.github.com>
2024-11-04 16:31:02 +0100

    amelioration  api

--- Interface/app.py
+++ Interface/app.py
@@ -1,25 +1,34 @@
 # app.py
 import streamlit as st
 from pages_uses import accueil,cartographie, prediction, evolution, navbar, topbar,  classification
+import  requests
 
 
-# Initialisation de l'état de la page si nécessaire
-if 'page' not in st.session_state:
-    st.session_state.page = 'Accueil'  # Page par défaut
 
-# Afficher le menu latéral
-#topbar.show_topbar
-navbar.show_menu()
+url = "https://performance-energetique-server.onrender.com/"
+response = requests.get(url)
+if response.status_code == 200:
+    # print("Le serveur est prêt.")
 
 
-# Affichage du contenu en fonction de la page sélectionnée
-if st.session_state.page == 'Accueil':
-    accueil.show()
-elif st.session_state.page == 'Cartographie':
-    cartographie.show()
-elif st.session_state.page == 'Prédiction':
-    prediction.show()
-elif st.session_state.page == 'Classification':
-    classification.show()
-elif st.session_state.page == 'Évolution':
-    evolution.show()
+
+    # Initialisation de l'état de la page si nécessaire
+    if 'page' not in st.session_state:
+        st.session_state.page = 'Accueil'  # Page par défaut
+
+    # Afficher le menu latéral
+    #topbar.show_topbar
+    navbar.show_menu()
+
+
+    # Affichage du contenu en fonction de la page sélectionnée
+    if st.session_state.page == 'Accueil':
+        accueil.show()
+    elif st.session_state.page == 'Cartographie':
+        cartographie.show()
+    elif st.session_state.page == 'Prédiction':
+        prediction.show()
+    elif st.session_state.page == 'Classification':
+        classification.show()
+    elif st.session_state.page == 'Évolution':
+        evolution.show()
